ee2a2caa0b25d4b592d2090721581562ee1fb99f
GannaAttia <gannaattia305@gmail.com>
2024-12-11 20:52:32 +0200

    .

---
+++ src/main/java/com/example/electricity_billing_system/Controllers/Admin/AdminMenuController.java
@@ -0,0 +1,18 @@
+package com.example.electricity_billing_system.Controllers.Admin;
+
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+public class AdminMenuController implements Initializable {
+    public Button view_stat_btn;
+    public Button view_bill_btn;
+    public Button manage_users_btn;
+    public Button logout_btn;
+
+    @Override
+    public void initialize(java.net.URL url, java.util.ResourceBundle resourceBundle) {
+
+    }
+}
+
+
---
+++ src/main/java/com/example/electricity_billing_system/Controllers/Admin/ViewStatsController.java
@@ -0,0 +1,11 @@
+package com.example.electricity_billing_system.Controllers.Admin;
+
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.ListView;
+import javafx.scene.text.Text;
+
+public class ViewStatsController {
+    public ChoiceBox select_region;
+    public Text total_collected_txt;
+    public ListView consumption_list;
+}
